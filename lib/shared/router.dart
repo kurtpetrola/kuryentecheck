8c45e145add0f08abcd316ae9b6617a8801e6f4a
Kurt Petrola <kurtguenpetrola2.0@gmail.com>
2026-01-10 01:30:04 +0800

    feat: add onboarding screen

--- lib/shared/router.dart
+++ lib/shared/router.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
 
 import '../features/report/report_screen.dart';
 import '../features/map/map_screen.dart';
 import '../features/feed/feed_screen.dart';
 import '../features/profile/profile_screen.dart';
 import 'scaffold_with_navbar.dart';
+import 'onboarding_screen.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
   final rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -14,7 +16,23 @@ final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     navigatorKey: rootNavigatorKey,
     initialLocation: '/report',
+    redirect: (context, state) async {
+      final prefs = await SharedPreferences.getInstance();
+      final hasSeenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
+
+      // Avoid redirecting if already on the onboarding screen
+      if (!hasSeenOnboarding) {
+        if (state.matchedLocation != '/onboarding') {
+          return '/onboarding';
+        }
+      }
+      return null;
+    },
     routes: [
+      GoRoute(
+        path: '/onboarding',
+        builder: (context, state) => const OnboardingScreen(),
+      ),
       StatefulShellRoute.indexedStack(
         builder: (context, state, navigationShell) {
           return ScaffoldWithNavBar(navigationShell: navigationShell);
@@ -55,5 +73,28 @@ final routerProvider = Provider<GoRouter>((ref) {
         ],
       ),
     ],
+    errorBuilder: (context, state) => const _ErrorRedirect(),
   );
 });
+
+class _ErrorRedirect extends StatefulWidget {
+  const _ErrorRedirect();
+
+  @override
+  State<_ErrorRedirect> createState() => _ErrorRedirectState();
+}
+
+class _ErrorRedirectState extends State<_ErrorRedirect> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      if (mounted) context.go('/report');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: CircularProgressIndicator()));
+  }
+}
